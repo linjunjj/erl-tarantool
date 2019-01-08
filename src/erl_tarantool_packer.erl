3576ac3421bf98387dd355a7047009095d3f9b67
linjun <1045735402@qq.com>
2019-01-08 22:13:22 +0800

    Add  uint data type

--- src/erl_tarantool_packer.erl
+++ src/erl_tarantool_packer.erl
@@ -25,3 +25,18 @@ pack_int(N) when N >= -16#800000000000 ->
 pack_int(N) ->
     throw({badarg, N}).
 
+%+++++++++
+%% deal with uint data type
+pack_uint(N) when N < 128 ->
+    <<2#0:1, N:7>>;
+pack_uint(N) when (N band 16#FF) =:= N ->
+    <<16#CC:8, N:8>>;
+pack_uint(N) when (N band 16#FFFF) =:= N ->
+    <<16#CD:8, N:16/big-signed-integer-unit:1>>;
+pack_uint(N) when (N band 16#FFFFFFFF) =:= N ->
+    <<16#CE:8, N:32/big-signed-integer-unit:1>>;
+pack_uint(N) when (N band 16#FFFFFFFFFFFFFFFF) =:=N ->
+    <<16#CF:8, N:64/big-signed-integer-unit:1>>;
+pack_uint(N) ->
+    throw({badarg, N}).
+
